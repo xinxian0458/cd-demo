75d275271a617044a8e452ec13022c734704ff18
Ben Lin <ben.lin@mesosphere.com>
2016-06-30 10:01:25 +0800

    Update marathon.json with ELB URL

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-bin/demo.py install http://52.10.217.201/
-bin/demo.py pipeline --branch=ben-demo-branch --password=zcTzRHzKcnN1Gg http://benlin-9b-publicsl-syk55lc0fs81-796060581.us-west-2.elb.amazonaws.com/ http://52.10.217.201/
+bin/demo.py install http://54.169.214.197/
+bin/demo.py pipeline --branch=ben-demo-branch --password=zcTzRHzKcnN1Gg http://benlin-sx-publicsl-11f91ind8i9kd-763837412.us-west-2.elb.amazonaws.com/ http://54.169.214.197/
